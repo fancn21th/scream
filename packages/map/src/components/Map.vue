086bb08737ff4953eda8cc69ba0f5d5d8c0970a5
fancn21th <fancn21th@gmail.com>
2023-11-05 13:08:01 +0800

    👌 IMPROVE: basic setup for reusable component of map

---
+++ packages/map/src/components/Map.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+<template>
+  <h1>Map</h1>
+</template>
+<style scoped></style>
--- packages/map/vite.config.js
+++ packages/map/vite.config.js
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { fileURLToPath, URL } from 'url';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'Map',
+      // the name of the output files when the build is run
+      fileName: 'map',
+    },
+    sourcemap: true,
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    ],
+  },
+});
