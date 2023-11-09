7e81b1f190321ef65409e60869a9d0ed7af5ffb5
fancn21th <fancn21th@gmail.com>
2023-11-09 12:29:17 +0800

    deploy

--- assets/TrunkA-b8026b93.js
+++
@@ -1,1 +0,0 @@
-import{r as o,o as a,c as n,a as e,u as t,L as s,F as c}from"./index-a931bafe.js";const l={__name:"TrunkA",setup(_){return(u,m)=>{const r=o("router-view");return a(),n(c,null,[e(t(s),{index:10}),e(r)],64)}}};export{l as default};
