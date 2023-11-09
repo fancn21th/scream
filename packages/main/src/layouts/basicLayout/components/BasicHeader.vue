e588a0d0a5ba20d68878a94fb547d062343d6c63
fancn21th <fancn21th@gmail.com>
2023-11-09 10:06:19 +0800

    👌 IMPROVE: 大屏规范化链接

--- packages/main/src/layouts/basicLayout/components/BasicHeader.vue
+++ packages/main/src/layouts/basicLayout/components/BasicHeader.vue
@@ -9,7 +9,18 @@ onBeforeMount(() => {
 </script>
 
 <template>
-  <div class="header"></div>
+  <div class="header">
+    <figure>
+      <a href="https://fancn21th.cn/zh/normalization/bigscreen/">
+        <img src="https://avatars.githubusercontent.com/u/4645715?v=4" alt="document" class="document" />
+      </a>
+    </figure>
+    <figure>
+      <a href="https://github.com/fancn21th/scream">
+        <img src="./assets/github-mark.svg" alt="project" class="project" />
+      </a>
+    </figure>
+  </div>
 </template>
 
 <style scoped>
@@ -17,5 +28,22 @@ onBeforeMount(() => {
   width: 100%;
   height: var(--top-row-height);
   border: 1px solid black;
+  padding: 0 20px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+}
+
+.project {
+  width: 50px;
+  height: 50px;
+}
+
+.document {
+  width: 50px;
+  height: 50px;
+  border-radius: 100%;
+  margin-right: 20px;
 }
 </style>
