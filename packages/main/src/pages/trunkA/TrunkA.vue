3f2a6a156eb29237bf9b9f9c69504beca3addf4f
fancn21th <fancn21th@gmail.com>
2023-11-12 13:21:34 +0800

    👌 IMPROVE: OOS 开发模式命令更新

--- packages/main/src/pages/trunkA/TrunkA.vue
+++ packages/main/src/pages/trunkA/TrunkA.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { Layer } from '@/components/layer';
+import { Map } from '@scream/map';
 </script>
 
 <template>
   <Layer :index="10">
     <!-- map layer -->
+    <Map />
   </Layer>
   <router-view></router-view>
 </template>
